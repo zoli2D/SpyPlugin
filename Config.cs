66688a025265ea7bea3027c54b37a00c4ae101b4
zoli2D <112851255+zoli2D@users.noreply.github.com>
2023-09-21 07:36:47 +0200

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace SpyPlugin
+{
+    /// <inheritdoc />
+    public class Config : IConfig
+    {
+        /// <inheritdoc />
+        [Description("Whether or not this Spy Role is enabled.")]
+        public bool IsEnabled { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether debug messages will be shown.
+        /// </summary>
+        [Description("Whether or not to display debug messages in the server console.")]
+        public bool Debug { get; set; }
+
+
+        /// <summary>
+        /// Role configs for Spy.
+        /// </summary>
+        [Description("A CI Agent between the MTFs")]
+        public SpyRole SpyRoleConfig { get; set; } = new();
+
+
+
+    }
+}
---
+++ EventHandlers.cs
@@ -0,0 +1,63 @@
+﻿namespace SpyPlugin
+{
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using Exiled.CustomRoles.API.Features;
+    using Exiled.Events.EventArgs.Player;
+    using Exiled.Events.EventArgs.Server;
+    using PlayerRoles;
+    using Exiled.API.Features.Roles;
+    using Exiled.CustomRoles.Events;
+
+
+    /// <summary>
+    /// Handles general events for this _056Plugin.
+    /// </summary>
+    public class EventHandlers
+    {
+        private readonly SpyPlugin _plugin;
+
+        internal EventHandlers(SpyPlugin plugin) => this._plugin = plugin;
+
+        public int x = 0;
+        public void OnJoin(JoinedEventArgs ev)
+        {
+            x = x + 1;
+        }
+        public void OnLeft(LeftEventArgs ev)
+        {
+            x = x - 1;
+        }
+
+
+        private bool HasSpyRoleSpawned = false;
+
+        private int SpawnChance = 0;
+
+        public void OnSpawned(SpawnedEventArgs ev)
+        {
+            if (Server.PlayerCount >= 12)
+            {
+                if (HasSpyRoleSpawned == false)
+                {
+                    SpawnChance = UnityEngine.Random.Range(1, 101);
+                    if (SpawnChance <= 10 && HasSpyRoleSpawned == false)
+                    {
+                        if (ev.Player.Role.Type == RoleTypeId.NtfSergeant)
+                        {
+                            HasSpyRoleSpawned = true;
+                            CustomRole.Get(typeof(SpyRole)).AddRole(ev.Player);
+                        }
+                    }
+
+                }
+            }
+        } 
+
+
+        internal void OnEndingRound(EndingRoundEventArgs ev)
+        {
+            HasSpyRoleSpawned = false;
+        }
+    }
+}
